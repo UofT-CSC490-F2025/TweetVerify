ad440abc110cbdf04f6c5d9a32cf052ef152e656
richardwang1236 <wangyc2005@qq.com>
2025-10-12 21:07:12 -0400

    1

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,20 +47,28 @@ resource "aws_security_group" "ec2_sg" {
 
 # create ec2
 resource "aws_instance" "my_ec2" {
-  ami           = var.ami_id      
+  ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_name    
+  key_name      = var.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  root_block_device {
+    volume_size = 50   
+    volume_type = "gp3" 
+    delete_on_termination = true
+  }
+
   user_data = <<-EOF
-  #!/bin/bash
-  cd /home/
-  yum update -y
-  yum install -y python3 python3-pip git
-  git clone https://github.com/UofT-CSC490-F2025/TweetVerify.git
-  cd TweetVerify
-  pip3 install --no-cache-dir -r requirements.txt
-  nohup sudo python3 -m src.app > app.log 2>&1 &
-EOF
+    #!/bin/bash
+    cd /home/
+    sudo yum update -y
+    sudo yum install -y python3 python3-pip git
+    git clone https://github.com/UofT-CSC490-F2025/TweetVerify.git
+    cd TweetVerify
+    pip3 install --no-cache-dir -r requirements.txt
+    nohup sudo python3 -m src.app > app.log 2>&1 &
+  EOF
+
   tags = {
     Name = "terraform-ec2"
   }

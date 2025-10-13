889d44de55a244098847ecdcbea788a3eb033670
richardwang1236 <wangyc2005@qq.com>
2025-10-12 21:31:36 -0400

    1

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,6 +35,13 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 
   egress {

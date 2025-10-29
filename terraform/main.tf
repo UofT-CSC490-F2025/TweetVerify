252b18796f8f3aea681a6f2acecede86cd1e89a0
richardwang1236 <wangyc2005@qq.com>
2025-10-29 15:20:10 -0400

    1

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,14 +88,21 @@ data "aws_db_snapshot" "latest_snapshot" {
 # -----------------------------
 resource "aws_db_instance" "tweetverify_db" {
   identifier              = "tweetverify-db"
-
-  snapshot_identifier     = data.aws_db_snapshot.latest_snapshot.id  
-
+  engine                  = "postgres"
+  engine_version          = "17.4"
   instance_class          = "db.t3.micro"
+  allocated_storage       = 20
+  storage_type            = "gp3"
+  username                = var.db_username
+  password                = var.db_password
+  db_name                 = "tweetverify"
+  port                    = 5432
   publicly_accessible     = false
+  skip_final_snapshot     = false
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+  multi_az                = false
+  backup_retention_period = 1
   deletion_protection     = false
-  skip_final_snapshot     = true
 
   tags = {
     Name = "tweetverify-db"

961f7b71b7956d744c60f1ae586e3b3dedfaaec4
richardwang1236 <wangyc2005@qq.com>
2025-11-25 22:09:35 -0500

    1

--- src/train.py
+++ src/train.py
@@ -1,7 +1,7 @@
 # ===== Standard Library =====
 import os
 import argparse
-
+import numpy as np
 # ===== Third-Party Libraries =====
 import torch
 import pandas as pd
@@ -20,7 +20,9 @@ from src.model.rnn import MyRNN
 from src.model.bert import BertClassifier
 from src.model.deberta import DebertaV3
 from src.model.roberta import MyRobertaForBinaryClassification
+from src.model.roberta_extra import Roberta_Extra
 from src.dataloader.bertdataset import BertDataset
+from src.dataloader.featuredataset import FeatureDataset
 from src.trainer.trainer import Trainer
 from src.evaluator.evaluator import Evaluator
 from src.utils.convert_indices import convert_indices
@@ -60,8 +62,7 @@ def main():
     parser.add_argument(
         "--model",
         type=str,
-        choices=["rnn", "lstm", "bert","deberta",'roberta'],
-        help="Model type [rnn, lstm, bert, deberta]"
+        help="Model type"
     )
     parser.add_argument(
         "--epochs", type=int, default=100, help="Number of training epochs"
@@ -80,10 +81,10 @@ def main():
     print(f"Model will be saved to: {args.output_path}", flush=True)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     set_all_seeds()
+    
+    model_w2v = Word2Vec.load("datasets/w2vmodel.model")
     human_token = pd.read_csv("datasets/human_token.csv")
     ai_token = pd.read_csv("datasets/ai_token.csv")
-    model_w2v = Word2Vec.load("datasets/w2vmodel.model")
-
     # Combine human token and ai token
     token = pd.concat([human_token, ai_token], ignore_index=True)
     # Shuffle and split the data
@@ -193,13 +194,68 @@ def main():
         acc = test_evaluator.accuracy(args.batch_size)
     elif args.model == "roberta":
         model_name = "FacebookAI/roberta-large"
-        model = MyRobertaForBinaryClassification.from_pretrained(model_name)
-        model = model.to(device)
+        model = MyRobertaForBinaryClassification.from_pretrained(model_name).to(device)
         tokenizer = AutoTokenizer.from_pretrained(model_name)
 
         train_dataset = BertDataset(X_train, y_train, tokenizer)
         val_dataset = BertDataset(X_val, y_val, tokenizer)
         test_dataset = BertDataset(X_test, y_test, tokenizer)
+
+        trainer = Trainer(
+            device,
+            model,
+            train_dataset,
+            val_dataset,
+            learning_rate=args.learning_rate,
+            num_epochs=args.epochs,
+            model_save_dir=args.output_path,
+            batch_size=args.batch_size,
+        )
+        train_loss, val_acc = trainer.train_model()
+
+        test_evaluator = Evaluator(model, test_dataset, device)
+        acc = test_evaluator.accuracy(args.batch_size)
+    elif args.model=='roberta_extra':
+        human_token = pd.read_csv("datasets/human_token_with_features.csv")
+        ai_token = pd.read_csv("datasets/ai_token_with_features.csv")
+        # Combine human token and ai token
+        token = pd.concat([human_token, ai_token], ignore_index=True)
+        token['ppl'] = token['ppl'].fillna(0)
+        token['burstiness'] = token['burstiness'].fillna(0)
+        feature_data = np.column_stack((
+            np.log1p(token['ppl'].values), 
+            np.log1p(token['burstiness'].values)
+        ))
+        # Shuffle and split the data
+        X_train, X_temp, y_train, y_temp, feat_train, feat_temp = train_test_split(
+            token["text"], 
+            token["label"], 
+            feature_data,  
+            test_size=0.3, 
+            random_state=42
+        )
+        
+        X_val, X_test, y_val, y_test, feat_val, feat_test = train_test_split(
+            X_temp, 
+            y_temp, 
+            feat_temp,     
+            test_size=0.5, 
+            random_state=42
+        )
+
+        # Reset indices to ensure continuous indexing for DataLoader
+        X_train = X_train.reset_index(drop=True)
+        y_train = y_train.reset_index(drop=True)
+        X_val = X_val.reset_index(drop=True)
+        y_val = y_val.reset_index(drop=True)
+        X_test = X_test.reset_index(drop=True)
+        y_test = y_test.reset_index(drop=True)
+        model_name = "FacebookAI/roberta-large"
+        model = Roberta_Extra.from_pretrained(model_name).to(device)
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        train_dataset = FeatureDataset(X_train, y_train, feat_train, tokenizer)
+        val_dataset = FeatureDataset(X_val, y_val, feat_val, tokenizer)
+        test_dataset = FeatureDataset(X_test, y_test, feat_test, tokenizer)
 
         trainer = Trainer(
             device,
